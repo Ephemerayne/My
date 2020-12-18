e791bbbf69bd83f979aac5b4329de4615325acb7
rockstar4095 <rockstar4095@gmail.com>
2020-12-18 16:07:52 +0300

    added modelReminder

---
+++ app/src/main/java/space/lala/nyxreminder/model/ReminderModel.java
@@ -0,0 +1,9 @@
+package space.lala.nyxreminder.model;
+
+import java.util.Date;
+
+public class ReminderModel {
+    public String title;
+    public String description;
+    public Date dateTime;
+}
